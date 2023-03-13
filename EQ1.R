9f50768660da6933f1f3bbc9a1121c86b90c4bc9
blankhall98 <blankhall@ciencias.unam.mx>
2023-03-13 17:00:32 -0600

    EQ1

--- EQ1.R
+++ EQ1.R
@@ -4,6 +4,8 @@ library(dplyr)
 library(ggplot2)
 library(tidyr)
 library(corrplot)
+library(olsrr)
+library(car)
 
 # read database
 jobcorps <- read_dta("data/jobcorps.dta")
@@ -34,20 +36,42 @@ jobcorps$ged_at_bline <- as.factor(jobcorps$ged_at_bline)
 jobcorps$anywlfr_bline <- as.factor(jobcorps$anywlfr_bline)
 jobcorps$alcohol_bline <- as.factor(jobcorps$alcohol_bline)
 
-# ols function
-model <- lm(data = jobcorps, formula = earn_lyr_bline ~ white + black + hispanic +
+# ols ---------------------
+
+# model 1: all possible variables
+model1 <- lm(data = jobcorps, formula = earn_lyr_bline ~ white + black + hispanic +
               eng_sec_language + havekids_at_bline + male + educlevel_at_bline +
               hsgrad_at_bline + ged_at_bline + mos_wrk_lyr_bline + wage_bline +
               anywlfr_bline + alcohol_bline + arrest_bline)
-summary(model)
-plot(model$residuals)
-logLik(model)
+summary(model1)
+# ### residuals
+plot(model1$residuals)
+hist(model1$residuals)
+# ### confidence intervals
+confint.lm(model1)
+# ### log likelihood
+logLik(model1)
+# ### VIF
+vif(model1)
 
-# ols function all significative
+# all possible combinations
+# this commands takes a lot of time to run!
+k <- ols_step_all_possible(model1)
+print(k)
+plot(k)
+
+# model 2: all significative
 model2 <- lm(data = jobcorps, formula = earn_lyr_bline ~ havekids_at_bline + male +
                mos_wrk_lyr_bline + wage_bline +
                anywlfr_bline + alcohol_bline + arrest_bline)
 summary(model2)
+# ### residuals
 plot(model2$residuals)
+hist(model2$residuals)
+# ### confidence intervals
+confint.lm(model2)
+# ### log likelihood
 logLik(model2)
+# ### VIF
+vif(model2)
 
